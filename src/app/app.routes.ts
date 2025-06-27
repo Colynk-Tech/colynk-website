3f4e3142daed17c35770d50bf9e5716cefd4ffd8
Pip <92968130+codepip55@users.noreply.github.com>
2025-06-27 12:06:19 +0200

    feat(pages): create experiences page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { OverOns } from './pages/over-ons/over-ons';
+import { Experience } from './pages/experience/experience';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'contact', component: Contact },
-  { path: 'over-ons', component: OverOns }
+  { path: 'over-ons', component: OverOns },
+  { path: 'ervaring', component: Experience }
 ];
