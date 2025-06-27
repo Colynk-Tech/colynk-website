7511ed58ef09670dff078765622e7e3a9bbe8caa
Pip <92968130+codepip55@users.noreply.github.com>
2025-06-27 11:44:45 +0200

    feat(pages): add about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
+import { OverOns } from './pages/over-ons/over-ons';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'contact', component: Contact }
+  { path: 'contact', component: Contact },
+  { path: 'over-ons', component: OverOns }
 ];
---
+++ src/app/pages/over-ons/over-ons.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OverOns } from './over-ons';
+
+describe('OverOns', () => {
+  let component: OverOns;
+  let fixture: ComponentFixture<OverOns>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [OverOns]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OverOns);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
