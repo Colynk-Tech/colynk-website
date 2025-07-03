d67e956a54f67a1d8eae652cd58d301beb11d3dc
Pip <92968130+codepip55@users.noreply.github.com>
2025-07-03 10:07:45 +0200

    fix(contact): fix contact form

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
 import { API_URL } from '../app.config';
+import { AlertService } from './alert';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,13 @@ import { API_URL } from '../app.config';
 export class Api {
   private apiUrl = inject(API_URL);
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private alertService: AlertService
   ) { }
 
   private handleError<T>(err: any, res: T, action: string): Observable<T> {
     console.error(err);
-    // this.alertService.add({ type: 'warning', message: err.error ? err.error.message : `Het is niet gelukt om '${action}' uit te voeren.` });
+    this.alertService.add({ type: 'warning', message: err.error ? err.error.message : `Het is niet gelukt om '${action}' uit te voeren.` });
     return of(res);
   }
 
@@ -22,7 +24,7 @@ export class Api {
   sendContactForm(name: string, email: string, message: string): Observable<any> {
     return this.http.post(this.apiUrl + '/email/contact', { name, email, message })
       .pipe(
-        catchError(err => this.handleError(err, null, 'contact'))
+        catchError(err => this.handleError(err, null, 'bericht versturen'))
       );
   }
 }
