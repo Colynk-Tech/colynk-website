7a9558f4b9abbd6ed114da2a5f68a45023d59414
Pip <92968130+codepip55@users.noreply.github.com>
2025-07-04 12:32:39 +0200

    feature(navbar): make hamburger icon on smaller devices

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,19 +1,45 @@
 import { Component } from '@angular/core';
 import { Button } from '../button/button';
 import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     Button,
+    NgClass,
   ],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss'
+  styleUrl: './navbar.scss',
+  animations: [
+    trigger('dropdownAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-1rem)' }),
+        animate('200ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('100ms ease-in', style({ opacity: 0, transform: 'translateY(-1rem)' })),
+      ]),
+    ])
+  ]
 })
 export class Navbar {
+  menuOpen = false;
+
   constructor(private router: Router) {
   }
+
   public navigateTo(page: string): void {
     this.router.navigate([`/${page}`]);
+  }
+
+  public toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  public closeMenu(): void {
+    this.menuOpen = false;
   }
 }
