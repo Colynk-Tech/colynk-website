2277132cc82ee1bdae1087f40486f11109f94e4d
Pip <92968130+codepip55@users.noreply.github.com>
2025-07-08 11:27:32 +0200

    feat(home): add packages for globe animation

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Button } from '../../components/button/button';
 import { Router } from '@angular/router';
 import { Seo } from '../../services/seo';
+import { animate, svg } from 'animejs';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { Seo } from '../../services/seo';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit {
+export class Home implements OnInit, AfterViewInit {
   constructor(
     private router: Router,
     private seoService: Seo,
@@ -21,6 +22,21 @@ export class Home implements OnInit {
         'Colynk, tech, website, cross-platform, development, design, hosting, domain',
     });
   }
+  ngAfterViewInit() {
+    animate('#paper-plane', {
+      ease: 'linear',
+      duration: 10000,
+      loop: true,
+      ...svg.createMotionPath('#orbit-path-1'),
+    });
+    animate('#circle-1', {
+      ease: 'linear',
+      duration: 10000,
+      loop: true,
+      ...svg.createMotionPath('#orbit-path-2'),
+    });
+  }
+
   public navigateTo(page: string): void {
     this.router.navigate([`/${page}`]);
   }
