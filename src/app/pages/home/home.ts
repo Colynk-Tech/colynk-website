8fc15026e45213504dce263c1fb42c7d4a192f4a
Pip <92968130+codepip55@users.noreply.github.com>
2025-07-09 06:43:45 +0200

    feat(home): animate globe

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Button } from '../../components/button/button';
 import { Router } from '@angular/router';
 import { Seo } from '../../services/seo';
-import { animate, svg } from 'animejs';
+import { createTimeline, svg } from 'animejs';
 
 @Component({
   selector: 'app-home',
@@ -23,18 +23,47 @@ export class Home implements OnInit, AfterViewInit {
     });
   }
   ngAfterViewInit() {
-    animate('#paper-plane', {
-      ease: 'linear',
-      duration: 10000,
-      loop: true,
-      ...svg.createMotionPath('#orbit-path-1'),
+    // animate('#paper-plane', {
+    //   ease: 'linear',
+    //   duration: 10000,
+    //   loop: true,
+    //   ...svg.createMotionPath('#orbit-path-1'),
+    // });
+    // animate('#circle-1', {
+    //   ease: 'linear',
+    //   duration: 10000,
+    //   loop: true,
+    //   ...svg.createMotionPath('#orbit-path-2'),
+    // });
+
+    const orbitPath1MotionPath = svg.createMotionPath('#orbit-path-1');
+    const orbitPath2MotionPath = svg.createMotionPath('#orbit-path-2');
+    const timeline = createTimeline({
+      defaults: {
+        ease: 'inOutSine',
+        duration: 10000,
+        loop: true,
+      },
     });
-    animate('#circle-1', {
-      ease: 'linear',
-      duration: 10000,
-      loop: true,
-      ...svg.createMotionPath('#orbit-path-2'),
+    timeline.add('#paper-plane', orbitPath1MotionPath, 100);
+
+    const timelinePath2 = createTimeline({
+      defaults: {
+        ease: 'inOutQuad',
+        duration: 5000,
+        loop: true,
+      },
     });
+    timelinePath2.add('#circle-1', orbitPath2MotionPath, 500);
+
+    const timelinePath3 = createTimeline({
+      defaults: {
+        ease: 'inOutQuad',
+        duration: 9000,
+        loop: true,
+      },
+    });
+    timelinePath2.add('#circle-2', orbitPath2MotionPath, 1500);
   }
 
   public navigateTo(page: string): void {
